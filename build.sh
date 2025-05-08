3975901f155f5954cb34a82d402502e6d031de5a
jorge <jorge@incsteps.com>
2025-05-08 15:54:21 +0200

    add pildora asciidoctor-intro

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker run -i --rm   --volume=$PWD:"/usr/app"   -u $(id -u ${USER}):$(id -g ${USER})   ggrossetie/asciidoctor-web-pdf:latest   --template-require ./jagedn/template.js -D pdfs  *.adoc
+asciidoctor-web-pdf   --template-require ./jagedn/template.js -D pdfs  *.adoc
 
 pdfunite pdfs/0-100-java-a-k8s-1.pdf pdfs/0-100-java-a-k8s-2.pdf pdfs/0-100-java-a-k8s.pdf
